e4f1dcda227d184963074a709581eee0b7126146
Anderson Lopes <andersonlopes017.al@gmail.com>
2024-06-18 18:57:42 -0400

    ✨ chore: Update Home component to receive props and add server-side data fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import camiseta2 from '../assets/camisetas/2.png'
 import camiseta3 from '../assets/camisetas/3.png'
 
 import 'keen-slider/keen-slider.min.css'
-export default function Home() {
+export default function Home(props) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
@@ -16,9 +16,13 @@ export default function Home() {
     }
   })
 
+
+
   return (
 
     <HomeContainer ref={sliderRef} className="keen-slider">
+      <pre>{JSON.stringify(props.list)}</pre>
+
       <Product className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt="" />
 
@@ -59,4 +63,13 @@ export default function Home() {
     </HomeContainer>
 
   )
+}
+
+export const getServerSideProps = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+  return {
+    props: {
+      list: [1, 2, 3]
+    },
+  }
 }
