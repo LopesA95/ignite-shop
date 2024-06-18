d9bfa605958c715dfe47d879f98c8701eb67de24
Anderson Lopes <andersonlopes017.al@gmail.com>
2024-06-18 15:45:52 -0400

    ✨ chore: Update app header to use Next.js Image component and add product images to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,33 @@
-import { styled } from '../styles'
+import Image from "next/image"
+import { HomeContainer, Product } from "../styles/pages/home"
 
-const Button = styled('button', {
-  color: 'white',
-  background: '$green300',
-  borderRadius: 4,
-  padding: '10px',
-  margin: '10px',
-  border: 'none',
-  cursor: 'pointer',
-
-  span: {
-    fontWeight: 'bold',
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.9)',
-  }
-})
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
 
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
-      <Button>
-        <span>teste</span>
-        Clique aqui
-      </Button>
+      <HomeContainer>
+        <Product>
+          <Image src={camiseta1} width={520} height={480} alt="" />
+
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79,90</span>
+
+          </footer>
+        </Product>
+        <Product>
+          <Image src={camiseta2} width={520} height={480} alt="" />
+
+          <footer>
+            <strong>Camiseta Y</strong>
+            <span>R$ 79,90</span>
+
+          </footer>
+        </Product>
+      </HomeContainer>
     </>
   )
 }
