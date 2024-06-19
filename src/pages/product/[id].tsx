4bac090141e13ed6a3c77aa566242bef8bdaa7b9
Anderson Lopes <andersonlopes017.al@gmail.com>
2024-06-19 15:04:14 -0400

    ✨ feat: Add product details and layout to Product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,10 +1,26 @@
+import Image from 'next/image'
 import { useRouter } from 'next/router'
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product'
 
 const Product = () => {
-	const router = useRouter()
-	const { id } = router.query
+	const { query } = useRouter()
 
-	return <div>Product ID: {id}</div>
+	return (
+		<ProductContainer>
+			<ImageContainer>
+				
+			</ImageContainer>
+
+			<ProductDetails>
+				<h1>Camiseta x</h1>
+				<span>R$ 79,90</span>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus distinctio quisquam quis vel sed odit minima animi dicta quia id, ea inventore. Illum at cupiditate expedita, accusamus ut laborum voluptate!</p>
+
+				<button>Comprar agora</button>
+			</ProductDetails>
+		</ProductContainer>
+	)
 }
 
 export default Product
